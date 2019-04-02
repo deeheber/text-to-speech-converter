cfee9d419dce47cc8bea04192d4b75199e117a4b
Danielle Heberling <daheber@gmail.com>
2019-04-02 13:54:24 -0700

    Update index.js
    Notes for ConvertToAudio function

--- src/ConvertToAudio/index.js
+++ src/ConvertToAudio/index.js
@@ -1,4 +1,13 @@
 exports.handler = async message => {
+  /*
+    Get the file information from the Table
+    Divide the file text into sections of 1000 characters (store in an array)
+    Loop through the array for each item
+      Send to synthesize the speech in Polly with output format of mp3
+      Save the synthesized speech to /tmp/fileId
+    Upload the /tmp/fileId to the FileStore bucket
+    Update the item in dynamoDB Table to have the URL to the file in S3
+  */
   console.log(message);
 
   return {};
