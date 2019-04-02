52488c398677cc2260d629a81186ef446b43058f
Danielle Heberling <daheber@gmail.com>
2019-04-02 13:44:56 -0700

    Update index.js
    Notes for GetFile

--- src/GetFile/index.js
+++ src/GetFile/index.js
@@ -1,4 +1,10 @@
 exports.handler = async message => {
+  /*
+  Is there a fileId?
+    - Y  table query for that specific file
+    - N table scan to return all files
+
+  */
   console.log(message);
 
   return {};
