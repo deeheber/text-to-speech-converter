2c6aba8dc0f31a589f2895db5571d2aa2a7382c1
Danielle Heberling <daheber@gmail.com>
2019-04-02 13:48:08 -0700

    Update index.js
    Notes for CreateNewFile function

--- src/CreateNewFile/index.js
+++ src/CreateNewFile/index.js
@@ -1,4 +1,10 @@
 exports.handler = async message => {
+  /*
+    Generate a file ID
+    Insert a new record into Table
+    { id, text, voice, status: 'PROCESSING' }
+    Invoke ConvertToAudioLambda (send the file ID
+  */
   console.log(message);
 
   return {};
