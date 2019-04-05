c228c2e18e1d29dcddb85fe111016eaf9b74fc2e
danielle <danielle@stackery.io>
2019-04-05 16:29:37 -0700

    CreateNewFile function code.

--- src/CreateNewFile/index.js
+++ src/CreateNewFile/index.js
@@ -1,11 +1,30 @@
-exports.handler = async message => {
-  /*
-    Generate a file ID
-    Insert a new record into Table
-    { id, text, voice, status: 'PROCESSING' }
-    Invoke ConvertToAudioLambda (send the file ID
-  */
-  console.log(message);
+const uuid = require('uuid/v4');
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-  return {};
-}
+exports.handler = async message => {
+  console.log(`CreateNewFile invoked  with  message: ${message}`);
+
+  const id = uuid();
+
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Item: {
+      id,
+      text: JSON.parse(message.text),
+      voice: JSON.parse(message.voice),
+      status: 'PROCESSING'
+    }
+  };
+
+  console.log(`Adding file metadata to table ${process.env.TABLE_NAME}`);
+  await dynamodb.put(params).promise();
+  console.log(`Metadata added to table, done`);
+
+  const lambda = new AWS.Lambda();
+
+  lambda.invoke({
+    FunctionName: process.env.FUNCTION_NAME,
+    Payload: JSON.stringify({ id })
+  });
+};
