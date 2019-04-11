a621886059cb9e476aaf818a07dbe85c268e97b6
danielle <danielle@stackery.io>
2019-04-10 21:07:26 -0700

    Add Polly into the mix.

--- src/ConvertToAudio/index.js
+++ src/ConvertToAudio/index.js
@@ -1,4 +1,5 @@
 const AWS = require('aws-sdk');
+const polly = new AWS.Polly();
 const s3 = new AWS.S3();
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const fs = require('fs');
@@ -12,7 +13,7 @@ exports.handler = async message => {
     Loop through the array for each item
       Send to synthesize the speech in Polly with output format of mp3
       Save the synthesized speech to /tmp/fileId
-    Upload the /tmp/fileId to the FileStore bucket
+    Upload the /tmp/fileId.mp3 to the FileStore bucket
     Update the item in dynamoDB Table to have the URL to the file in S3 and the status to COMPLETE
   */
   console.log(`ConvertToAudio invoked with message: ${JSON.stringify(message, null, 2)}`);
@@ -20,16 +21,24 @@ exports.handler = async message => {
   let response;
 
   try {
-    const writeFile = await writeFilePromise(`/tmp/${message.id}.txt`, `${message.text}`);
+    const pollyFile = await polly.synthesizeSpeech({
+      OutputFormat: 'mp3',
+      Text: `${message.text}`,
+      TextType: 'text',
+      VoiceId: `${message.voice}`
+    }).promise();
+
+    console.log('SUCCESS converting text to audio file: ', pollyFile);
+
+    const writeFile = await writeFilePromise(`/tmp/${message.id}.mp3`, pollyFile.AudioStream);
 
     console.log('SUCCESS writing file: ', writeFile);
 
     const uploadToS3 = await s3.putObject({
       ACL: 'public-read',
       Bucket: process.env.BUCKET_NAME,
-      Key: `${message.id}.txt`,
-      Body: fs.createReadStream(`/tmp/${message.id}.txt`),
-      ContentType: 'application/zip'
+      Key: `${message.id}.mp3`,
+      Body: fs.createReadStream(`/tmp/${message.id}.mp3`)
     }).promise();
 
     console.log('SUCCESS uploading to S3: ', uploadToS3);
@@ -40,7 +49,7 @@ exports.handler = async message => {
       UpdateExpression: 'SET #file_status = :status, #s3_url = :url',
       ExpressionAttributeValues: {
         ':status': 'COMPLETE',
-        ':url': `https://s3.amazonaws.com/${process.env.BUCKET_NAME}/${message.id}.txt`
+        ':url': `https://s3.amazonaws.com/${process.env.BUCKET_NAME}/${message.id}.mp3`
       },
       ExpressionAttributeNames: {
         '#file_status': 'status',
