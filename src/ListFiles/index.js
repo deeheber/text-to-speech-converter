839f4f39fdbf94217893535d30c4d000408b2634
danielle <danielle@stackery.io>
2019-11-05 13:49:56 -0800

    Sort dynamoDB ListFiles results by createdAt

--- src/ListFiles/index.js
+++ src/ListFiles/index.js
@@ -13,8 +13,15 @@ exports.handler = async message => {
   };
 
   try {
-    // TODO add looping with `ExclusiveStartKey` and `LastEvaluatedKey` for larger tables
-    response = await dynamodb.scan(listParams).promise();
+    /* TODO: add looping with `ExclusiveStartKey` and `LastEvaluatedKey` for larger tables */
+    const { Items } = await dynamodb.scan(listParams).promise();
+    console.log('ORIGINAL RESULTS FROM DB ', Items);
+    /* TODO: Figure out if there's a way to sort scan results from dynamo
+    * Initial search showed this may not be an option and/or is hard to implement
+    */
+    const sortedItems = Items.sort((a, b) => b.createdAt - a.createdAt);
+    console.log('SORTED RESULTS FROM DB ', sortedItems);
+    response = { Items: sortedItems };
     statusCode = 200;
   } catch (err) {
     console.log('An error occurred scanning the table: ', err);
